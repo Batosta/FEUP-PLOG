19a04b20994ea2422765d29dee3090b6a7f6c2ed
João Ferreira <up201605330@g.uporto.pt>
2018-12-06 00:23:55 +0000

    Board display finished. Houses appear and with the corresponding numbers

--- House Puzzle/display.pl
+++ House Puzzle/display.pl
@@ -11,43 +11,55 @@ printBoard([H|T]) :-
 	length(H, Col),
 	nl, write('      '),
 	separation1(Col), nl,
-	display_board([H|T], Col),
+	display_board([H|T], Col, 1),
 	write('      '),
 	separation1(Col), nl, nl.
 
 % Displays the whole puzzle
-display_board([], _).
-display_board([H|T], Col) :-
-	write('     | '),
-	printRow(H),
+display_board([], _, _).
+display_board([H|T], Col, Number) :-
+
+	write('     |  '),
+	printRow(H, Number, Aux),
 	write('  |'), nl,
 
 	write('     |'),
 	separation2(Col),
 	write('|'), nl,
 
-	display_board(T, Col).
+	display_board(T, Col, Aux).
 
 
 % Prints a single row
-printRow([]).
-printRow([H|T]) :-
+printRow([], Number, Number).
+printRow([H|T], Number, Aux) :-
 	H = 1,
-	write(' H '),
-	printRow(T);
-	write('   '),
-	printRow(T).
+		printHouse(Number),
+		Number1 is Number + 1,
+		printRow(T, Number1, Aux);
+		
+		write('    '),
+		printRow(T, Number, Aux).
 
+
+printHouse(Number) :-
+	Number @< 10,
+		write(' H'),
+		write(Number),
+		write(' ');
+
+		write(' H'),
+		write(Number).
 
 separation1(-1).
 separation1(C) :-
-	write('---'),
+	write('----'),
 	C1 is C - 1,
 	separation1(C1).
 
 % Prints the separation between 2 rows
 separation2(-1).
 separation2(C) :-
-	write('   '),
+	write('    '),
 	C1 is C - 1,
 	separation2(C1).
